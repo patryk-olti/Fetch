0f6fdfcddf8aa241d15d77da29cba445fcc4edc9
Patryk Oltuch <95oltuchpatryk@gmail.com>
2021-11-03 11:31:40 +0100

    commit

---
+++ src/Containers/UsersContainer.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+const UsersContainer = () => {
+
+    const [ userList, setUserList ] = useState([]);
+
+ 
+    return(
+        <div>   
+            ello
+
+            { console.log(userList) }
+        </div>
+    )
+}
+
+export default UsersContainer;
